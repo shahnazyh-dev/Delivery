5c417924e9f655c5ad42f83fca6d553510fd8e37
Shahnaz Y. Hajiyeva <shahnazyh@code.local>
2026-04-26 13:23:23 +0400

    fixed

--- Delivery/Delivery/obj/Release/net8.0/Delivery.AssemblyInfo.cs
+++ Delivery/Delivery/obj/Release/net8.0/Delivery.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Delivery")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+739b837dde4958d5203ed328f596c360d05e2fb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Delivery")]
